d1c659512f273f7cdb83493aec696781fbf22557
JOSEMLI <JOSEMLI@example.com>
2020-01-21 13:23:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mi_antivivus/Servicio.java
@@ -0,0 +1,14 @@
+package com.example.mi_antivivus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Servicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
